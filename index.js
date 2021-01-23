f02e6afc44c97b92d84ebf47239803e8c9b08a61
sunxiaoqi <17319051427@163.com>
2021-01-23 14:36:27 +0800

    _3

--- index.js
+++ index.js
@@ -6,5 +6,32 @@ $('.english,.chinese').on('click',function(e){
     e.stopPropagation()
     $(this).css("background",'#F9FAFC')
     $(this).siblings().css("background",'#fff')
-    $('.switch_language').hide()
+    // $('.switch_language').hide()
+})
+$('.cont_1 .rect_click').on('click',function(e){
+    // // console.log(this)
+    // e = e || event
+    // e.stopPropagation()
+    // e.cancelBubble = true
+    if($(this).context.className.baseVal.includes('unactive_1')){
+        $('.cont_1 .active_1').show()
+        $('.cont_1 .unactive_1').hide()
+        $('.cont_1 .unactive_2').show()
+        $('.cont_1 .active_2').hide()
+    } else if($(this).context.className.baseVal.includes('active_1')) {
+        $('.cont_1 .active_1').show()
+        $('.cont_1 .unactive_1').hide()
+        $('.cont_1 .unactive_2').show()
+        $('.cont_1 .active_2').hide()
+    }else if($(this).context.className.baseVal.includes('active_2')) {
+        $('.cont_1 .unactive_1').show()
+        $('.cont_1 .active_1').hide()
+        $('.cont_1 .active_2').show()
+        $('.cont_1 .unactive_2').hide()
+    }else if($(this).context.className.baseVal.includes('unactive_2')) {
+        $('.cont_1 .unactive_1').show()
+        $('.cont_1 .active_1').hide()
+        $('.cont_1 .active_2').show()
+        $('.cont_1 .unactive_2').hide()
+    }
 })
